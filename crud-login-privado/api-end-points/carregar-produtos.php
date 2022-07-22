9277d8885f7cec5221e378295f8645e5b9b2570c
Higor Muros <96095355+higormuros@users.noreply.github.com>
2022-07-22 15:47:18 -0300

    Passado e fortemente alterado em public

--- crud-login-privado/api-end-points/carregar-produtos.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require '../../../crud-login-privado/login/conexao.php';
-require '../../../crud-login-privado/login/servico.php';
-$conexao=new conexao();
-$servico=new servico($conexao);
-$produtos=$servico->recuperarProdutos();
-?>
